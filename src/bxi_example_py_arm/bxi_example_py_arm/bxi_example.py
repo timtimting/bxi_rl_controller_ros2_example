85d5336c242bb7b9161f15e10d7d0ce936cdf921
liufq <popsay@163.com>
2025-09-12 16:35:26 +0800

    1.添加手臂控制示例以及urdf文件

---
+++ src/bxi_example_py_arm/bxi_example_py_arm/bxi_example.py
@@ -0,0 +1,219 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.qos import QoSProfile, qos_profile_sensor_data
+from rclpy.time import Time
+import communication.msg as bxiMsg
+import communication.srv as bxiSrv
+import nav_msgs.msg 
+import sensor_msgs.msg
+from threading import Lock
+import numpy as np
+# import torch
+import time
+import sys
+import os
+import math
+from collections import deque
+from std_msgs.msg import Header
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import JointState
+
+robot_name = "elf25"
+
+dof_num = 14
+
+joint_name = (
+    "l_shld_y_joint",   # 左臂_肩关节_y轴
+    "l_shld_x_joint",   # 左臂_肩关节_x轴
+    "l_shld_z_joint",   # 左臂_肩关节_z轴
+    "l_elb_y_joint",   # 左臂_肘关节_y轴
+    "l_elb_z_joint",   # 左臂_肘关节_y轴
+    "l_wrist_y_joint",
+    "l_wrist_x_joint",
+ 
+    "r_shld_y_joint",   # 右臂_肩关节_y轴   
+    "r_shld_x_joint",   # 右臂_肩关节_x轴
+    "r_shld_z_joint",   # 右臂_肩关节_z轴
+    "r_elb_y_joint",    # 右臂_肘关节_y轴
+    "r_elb_z_joint",    # 右臂_肘关节_y轴
+    "r_wrist_y_joint",
+    "r_wrist_x_joint",
+    )   
+
+joint_nominal_pos = np.array([   # 指定的固定关节角度
+    0,0,0,-1,0,0,0,     # 左臂放在大腿旁边 (Y=0 肩平, X=0 前后居中, Z=0 不旋转, 肘关节微弯)
+    0,0,0,-1,0,0,0,
+    ],    # 右臂放在大腿旁边 (Y=0 肩平, X=0 前后居中, Z=0 不旋转, 肘关节微弯)
+    dtype=np.float32)
+
+joint_kp = np.array([     # 指定关节的kp，和joint_name顺序一一对应
+    30,30,10,30,10,10,10,
+    30,30,10,30,10,10,10,
+    ], dtype=np.float32)
+
+joint_kd = np.array([  # 指定关节的kd，和joint_name顺序一一对应
+    1,1,0.8,1,0.8,0.8,0.8,
+    1,1,0.8,1,0.8,0.8,0.8,
+    ], dtype=np.float32)
+
+class BxiExample(Node):
+
+    def __init__(self):
+
+        super().__init__('bxi_example_py')
+        
+        self.declare_parameter('/topic_prefix', 'default_value')
+        self.topic_prefix = self.get_parameter('/topic_prefix').get_parameter_value().string_value
+        print('topic_prefix:', self.topic_prefix)
+
+        qos = QoSProfile(depth=1, durability=qos_profile_sensor_data.durability, reliability=qos_profile_sensor_data.reliability)
+        
+        self.act_pub = self.create_publisher(bxiMsg.ActuatorCmds, self.topic_prefix+'actuators_cmds', qos)  # CHANGE
+        self.joint_sub = self.create_subscription(sensor_msgs.msg.JointState, self.topic_prefix+'joint_states', self.joint_callback, qos)
+
+        self.rest_srv = self.create_client(bxiSrv.RobotReset, self.topic_prefix+'robot_reset')
+        self.sim_rest_srv = self.create_client(bxiSrv.SimulationReset, self.topic_prefix+'sim_reset')
+        
+        self.timer_callback_group_1 = MutuallyExclusiveCallbackGroup()
+
+        self.lock_in = Lock()
+        self.lock_ou = self.lock_in #Lock()
+        self.qpos = np.zeros(dof_num,dtype=np.double)
+        self.qvel = np.zeros(dof_num,dtype=np.double)
+
+        self.step = 0
+        self.loop_count = 0
+        self.dt = 0.01  # loop @100Hz
+        self.timer = self.create_timer(self.dt, self.timer_callback, callback_group=self.timer_callback_group_1)
+
+    def timer_callback(self):
+        
+        # ptyhon 与 rclpy 多线程不太友好，这里使用定时间+简易状态机运行a
+        if self.step == 0:
+            self.robot_rest(1, False) # first reset
+            print('robot reset 1!')
+            self.step = 1
+            return
+        elif self.step == 1 and self.loop_count >= (10./self.dt): # 延迟10s
+            self.robot_rest(2, False) # first reset
+            print('robot reset 2!')
+            self.loop_count = 0
+            self.step = 2
+            return
+        
+        if self.step == 1:
+            soft_start = self.loop_count/(1./self.dt) # 1秒关节缓启动
+            if soft_start > 1:
+                soft_start = 1
+                
+            soft_joint_kp = joint_kp * soft_start
+                
+            msg = bxiMsg.ActuatorCmds()
+            msg.header.frame_id = robot_name
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.actuators_name = joint_name
+            msg.pos = joint_nominal_pos.tolist()
+            # msg.pos = np.zeros(dof_num, dtype=np.float32).tolist()
+            msg.vel = np.zeros(dof_num, dtype=np.float32).tolist()
+            msg.torque = np.zeros(dof_num, dtype=np.float32).tolist()
+            msg.kp = soft_joint_kp.tolist()
+            msg.kd = joint_kd.tolist()
+            self.act_pub.publish(msg)
+            
+        elif self.step == 2:
+            with self.lock_in:
+                q = self.qpos
+                dq = self.qvel
+            
+            count_lowlevel = self.loop_count
+        
+            qpos = joint_nominal_pos.copy()
+            qpos[3] += np.sin(count_lowlevel * self.dt / 2. * np.pi * 2) * 0.5
+            qpos[10] += -np.sin(count_lowlevel * self.dt /2. * np.pi * 2) * 0.5
+            
+            vel = np.zeros(dof_num, dtype=np.float32)
+            torque = np.zeros(dof_num, dtype=np.float32)
+            
+            msg = bxiMsg.ActuatorCmds()
+            msg.header.frame_id = robot_name
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.actuators_name = joint_name
+            msg.pos = qpos.tolist()
+            msg.vel = vel.tolist()
+            msg.torque = torque.tolist()
+            msg.kp = joint_kp.tolist()
+            msg.kd = joint_kd.tolist()
+            self.act_pub.publish(msg)
+
+        self.loop_count += 1
+    
+    def robot_rest(self, reset_step, release):
+        req = bxiSrv.RobotReset.Request()
+        req.reset_step = reset_step
+        req.release = release
+        req.header.frame_id = robot_name
+    
+        while not self.rest_srv.wait_for_service(timeout_sec=1.0):
+            print('service not available, waiting again...')
+            
+        self.rest_srv.call_async(req)
+        
+    def sim_robot_rest(self):        
+        req = bxiSrv.SimulationReset.Request()
+        req.header.frame_id = robot_name
+
+        base_pose = Pose()
+        base_pose.position.x = 0.0
+        base_pose.position.y = 0.0
+        base_pose.position.z = 1.0
+        base_pose.orientation.x = 0.0
+        base_pose.orientation.y = 0.0
+        base_pose.orientation.z = 0.0
+        base_pose.orientation.w = 1.0        
+
+        joint_state = JointState()
+        joint_state.name = joint_name
+        joint_state.position = np.zeros(dof_num, dtype=np.float32).tolist()
+        joint_state.velocity = np.zeros(dof_num, dtype=np.float32).tolist()
+        joint_state.effort = np.zeros(dof_num, dtype=np.float32).tolist()
+        
+        req.base_pose = base_pose
+        req.joint_state = joint_state
+    
+        while not self.sim_rest_srv.wait_for_service(timeout_sec=1.0):
+            print('service not available, waiting again...')
+            
+        self.sim_rest_srv.call_async(req)
+    
+    def joint_callback(self, msg):
+        joint_pos = msg.position
+        joint_vel = msg.velocity
+        joint_tor = msg.effort
+        
+        with self.lock_in:
+            self.qpos = np.array(joint_pos[:])
+            self.qvel = np.array(joint_vel[:])
+            
+def main(args=None):
+   
+    time.sleep(5)
+    
+    rclpy.init(args=args)
+    node = BxiExample()
+    
+    executor = MultiThreadedExecutor(num_threads=3)
+    executor.add_node(node)
+    
+    try:
+        executor.spin()
+    finally:
+        executor.shutdown()
+        node.destroy_node()
+        
+    rclpy.shutdown()
+        
+if __name__ == '__main__':
+    main()
+    
---
+++ src/bxi_example_py_arm/launch/example_launch_hw.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    return LaunchDescription(
+        [
+            Node(
+                package="hardware_arm",
+                executable="hardware_arm",
+                name="hardware_arm",
+                output="screen",
+                parameters=[
+                ],
+                emulate_tty=True,
+                arguments=[("__log_level:=debug")],
+            ),
+
+            Node(
+                package="bxi_example_py_arm",
+                executable="bxi_example_py_arm",
+                name="bxi_example_py_arm",
+                output="screen",
+                parameters=[
+                    {"/topic_prefix": "hardware/"},
+                ],
+                emulate_tty=True,
+                arguments=[("__log_level:=debug")],
+            ),
+        ]
+    )
